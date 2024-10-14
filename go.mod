1aae1c8179525b6d667de7b01d6526a7d90f6926
Nikita Bakarev <nic231731@yandex.ru>
2024-10-14 12:24:26 +0300

    feat: create pg client

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/georgysavva/scany/v2 v2.1.3
 	github.com/golang/protobuf v1.5.4
 	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.65.0
---
+++ internal/client/db/pg/pg.go
@@ -0,0 +1,74 @@
+package pg
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/NikolosHGW/auth/internal/client/db"
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type pg struct {
+	dbc *pgxpool.Pool
+}
+
+// NewPG - конструктор реализации постгрес клиента.
+func NewPG(dbc *pgxpool.Pool) db.DB {
+	return &pg{dbc: dbc}
+}
+
+func (p *pg) ScanOneContext(ctx context.Context, dest any, q db.Query, args ...any) error {
+	logQuery(ctx, q, args...)
+
+	row, err := p.QueryContext(ctx, q, args...)
+	if err != nil {
+		return fmt.Errorf("ошибка ScanOneContext: %w", err)
+	}
+
+	return pgxscan.ScanOne(dest, row)
+}
+
+func (p *pg) ScanAllContext(ctx context.Context, dest any, q db.Query, args ...any) error {
+	logQuery(ctx, q, args...)
+
+	rows, err := p.QueryContext(ctx, q, args...)
+	if err != nil {
+		return fmt.Errorf("ошибка ScanAllContext: %w", err)
+	}
+
+	return pgxscan.ScanAll(dest, rows)
+}
+
+func (p *pg) ExecContext(ctx context.Context, q db.Query, args ...any) (pgconn.CommandTag, error) {
+	logQuery(ctx, q, args...)
+
+	return p.dbc.Exec(ctx, q.QueryRaw, args...)
+}
+
+func (p *pg) QueryContext(ctx context.Context, q db.Query, args ...any) (pgx.Rows, error) {
+	logQuery(ctx, q, args...)
+
+	return p.dbc.Query(ctx, q.QueryRaw, args...)
+}
+
+func (p *pg) QueryRowContext(ctx context.Context, q db.Query, args ...any) pgx.Row {
+	logQuery(ctx, q, args...)
+
+	return p.dbc.QueryRow(ctx, q.QueryRaw, args...)
+}
+
+func (p *pg) Ping(ctx context.Context) error {
+	return p.dbc.Ping(ctx)
+}
+
+func (p *pg) Close() {
+	p.dbc.Close()
+}
+
+func logQuery(ctx context.Context, q db.Query, args ...any) {
+	log.Println(ctx, fmt.Sprintf("sql: %s", q.Name), fmt.Sprintf("query: %s", q.QueryRaw), args, ". sql end.")
+}
